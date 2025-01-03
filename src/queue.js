9ddd058092b44bbfbf9abd05b6f3b64ca72e2e1c
Elena Khadasevich <camatsu90@mail.ru>
2025-01-03 03:03:35 +0300

    feat: add queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,22 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+ }
+
+ class LinkedList{
+  constructor(head = null){
+    this.head = head;
+    this.size = 0;
+  }
+
+  add(value)
+ {
+    let current;
+    if (this.head == null)
+        this.head = new Node(value);
+    else {
+        current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = new Node(value);
+    }
+  this.size += 1;
+ }
+
+ removeKFromList() {
+  if(this.head !== null){
+    let current = this.head;
+    this.head = this.head.next;
+    this.size -= 1;
+    return current.value;
+  }
+  return 1;
+}
+}
+
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+   this.list = new LinkedList();
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+   getUnderlyingList() {
+    return this.list.head;
+   }
+ 
+   enqueue(value) {
+   this.list.add(value);
+   }
+ 
+   dequeue() {
+    return this.list.removeKFromList();
+   }
 }
 
 module.exports = {
